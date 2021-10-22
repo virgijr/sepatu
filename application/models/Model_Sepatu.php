bad105a50434e89f750a6ace074e40cd9c572cb2
virgijr <12200479@bsi.ac.id>
2021-10-22 15:50:46 +0700

    bebas ajalah

---
+++ application/models/Model_Sepatu.php
@@ -0,0 +1,15 @@
+<?php
+class Model_Latihan1 extends CI_Model
+{
+    public $nilai1, $nilai2, $hasil;
+
+    public function getSepatu()
+    {
+        $sepatu = [
+            ['merek' => '', 'harga' => ''],
+            ['merek' => '', 'harga' => ''],
+
+
+        ];
+    }
+}
